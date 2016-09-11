35dcebe3130a3887779a044ed4f2a861eef2d1e5
chinh1 <chinguyenhuu15101992@gmail.com>
2016-09-12 06:46:46 +0700

    load file

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+
+//        giai phuong trinh bac 2: ax^2 + bx + c= 0
+        System.out.println("Bài tập nhập tên");
+        Scanner scanner = new Scanner (System.in);
+        System.out.println(" Mời bạn nhập tên của mình vào:");
+        String ten = scanner.nextLine();
+        System.out.println("xin chao " + ten);
+
+
+
+
+    }
+}
