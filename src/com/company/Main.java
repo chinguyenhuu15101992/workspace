d6c453cc6d913fbf62a2e8c2f20f652dda95492b
chinh1 <chinguyenhuu15101992@gmail.com>
2016-09-15 15:03:24 +0700

    all

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -52,31 +52,31 @@ public class Main {
 //            System.out.println("chào buổi chiều");
 
         /* Bài tập giải bài toán phương trình bậc 2 ax^2+bx+c=0 */
-        System.out.println("Giải bài toán phương trình bậc 2 ax^2+bx+c=0");
-        Scanner variable = new Scanner(System.in);
-        System.out.println("giá trị hằng số a là:");
-        float a = variable.nextInt();
-        System.out.println("gias trị hằng số b là:");
-        float b = variable.nextInt();
-        System.out.println("giá trị hằng số c là:");
-        float c = variable.nextInt();
-        float delta = b*b - 4*a*c;
-        if (delta < 0) {
-            System.out.println("phương trình vô nghiệm");
-        }
-        else if ( delta == 0) {
-            float x = (-b)/(2*a);
-            System.out.println ("phương trình có nghiệm là:");
-        }
-        else {
-            double x1 = ((-b)+sqrt(delta))/(2*a);
-            double x2 = ((-b)-sqrt(delta))/(2*a);
-            System.out.println("phương trình có hai nghiệm đó là");
-            System.out.println("nghiệm đầu tiên là x1= "+x1);
-            System.out.println(" nghiệm thứ 2 là x2= "+x2);
-            System.out.println(" Thank you so much");
-
-        }
+//        System.out.println("Giải bài toán phương trình bậc 2 ax^2+bx+c=0");
+//        Scanner variable = new Scanner(System.in);
+//        System.out.println("giá trị hằng số a là:");
+//        float a = variable.nextInt();
+//        System.out.println("gias trị hằng số b là:");
+//        float b = variable.nextInt();
+//        System.out.println("giá trị hằng số c là:");
+//        float c = variable.nextInt();
+//        float delta = b*b - 4*a*c;
+//        if (delta < 0) {
+//            System.out.println("phương trình vô nghiệm");
+//        }
+//        else if ( delta == 0) {
+//            float x = (-b)/(2*a);
+//            System.out.println ("phương trình có nghiệm là:");
+//        }
+//        else {
+//            double x1 = ((-b)+sqrt(delta))/(2*a);
+//            double x2 = ((-b)-sqrt(delta))/(2*a);
+//            System.out.println("phương trình có hai nghiệm đó là");
+//            System.out.println("nghiệm đầu tiên là x1= "+x1);
+//            System.out.println(" nghiệm thứ 2 là x2= "+x2);
+//            System.out.println(" Thank you so much");
+//
+//        }
 
             /* Bài tập tính lương cho nhân viên*/
 
@@ -85,27 +85,27 @@ public class Main {
 //        float
 
 
-//        Scanner input = new Scanner(System.in);
-//
-//
-//            System.out.println("nhân viên đạt loại:");
-//            String cate = input.nextLine();
-//            System.out.println("nhân viên mức lương là:");
-//            int salary = input.nextInt();
-//
-//
-////            String a,b;
-//
-//            if (cate.equals("a")) {
-//                int x = salary + 3000;
-//                System.out.println("người nhân viên nhận được lương là: " + x);
-//            } else if (cate.equals("b")) {
-//                int x = salary + 2000;
-//                System.out.println("người nhân viên nhận được lương là: " + x);
-//            } else {
-//                int x = salary + 1000;
-//                System.out.println("người nhân viên nhận được lương là: " + x);
-//            }
+        Scanner input = new Scanner(System.in);
+
+
+            System.out.println("nhân viên đạt loại:");
+            String cate = input.nextLine();
+            System.out.println("nhân viên mức lương là:");
+            int salary = input.nextInt();
+
+
+//            String a,b;
+
+            if (cate.equals("a")) {
+                int x = salary + 3000;
+                System.out.println("người nhân viên nhận được lương là: " + x);
+            } else if (cate.equals("b")) {
+                int x = salary + 2000;
+                System.out.println("người nhân viên nhận được lương là: " + x);
+            } else {
+                int x = salary + 1000;
+                System.out.println("người nhân viên nhận được lương là: " + x);
+            }
 
 
 
